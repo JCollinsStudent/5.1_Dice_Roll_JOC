3944fe18d169dc0b624f24ee2b580a6801a6b7e5
jcollinsstudent <joshuaolincollins@gmail.com>
2021-09-23 14:09:36 -0400

    Initial commit

---
+++ RecDieRoller.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+/**
+ * The RecDieRoller class contains a constructor and the roll method.
+ *
+ * @author Joshua Collins
+ * @version 1.0
+ * @since 2021-09-23
+ */
+public class RecDieRoller {
+
+	/**
+	 * The constructor to instantiate a new die roller.
+	 */
+	public RecDieRoller() {
+
+	}
+
+	/**
+	 * The roll method generates a random integer from 1-6, prints the integer, then calls itself if the integer is not equal to 2.
+	 */
+	public void roll() {
+		Random rand = new Random();
+		int num = rand.nextInt(6-1+1) + 1;
+		System.out.println(num);
+		if (num != 2) {
+			roll();
+		}
+	}
+}
---
+++ RecDieRollerDriver.java
@@ -0,0 +1,19 @@
+/**
+ * The RecDieRollerDriverclass contains the main method which instantiates a RecDieRoller and calls the roll method.
+ *
+ * @author Joshua Collins
+ * @version 1.0
+ * @since 2021-09-23
+ */ 
+public class RecDieRollerDriver {
+
+	/**
+	 * The main method instantiates a new die roller and performs the roll.
+	 *
+	 * @param args
+	 */ 
+	public static void main(String[] args) {
+		RecDieRoller dieRoller = new RecDieRoller();
+		dieRoller.roll();
+	}
+}
